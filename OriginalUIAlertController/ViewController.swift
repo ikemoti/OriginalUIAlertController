a25daabe139d10c3c251a1d31d2a809e13fdc9df
ikemoti <58681457+ikemoti@users.noreply.github.com>
2020-05-11 14:44:38 +0900

    Initial Commit

---
+++ OriginalUIAlertController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OriginalUIAlertController
+//
+//  Created by Sousuke Ikemoto on 2020/05/11.
+//  Copyright © 2020 Sousuke Ikemoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
